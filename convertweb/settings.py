aa05f79dc1097d093d083d17bed9d952077cc6dc
Binod Nagarkoti <Binod1365>
2019-06-07 05:08:15 +0545

    voting complete and class based generic views.

--- convertweb/settings.py
+++ convertweb/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'convertweb.urls'
 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+
+]
